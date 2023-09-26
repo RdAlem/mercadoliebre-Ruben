9d745073bc4eb712e6f5e090dbdd8cc631d1258a
Ruben <alem.ruben@gmail.com>
2023-09-25 22:51:47 -0300

    pag register

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
 app.listen(port, () =>{
     console.log(`Servidor iniciado en http://localhost:${port}`);
 });
